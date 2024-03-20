327ba80d0630503d271092a84906756c400e2f37
Mario Theodoridis <mario.theodoridis@regify.com>
2024-03-20 11:02:26 +0100

    update logging implementation

--- examples/datause.c
+++ examples/datause.c
@@ -36,6 +36,8 @@
 // The performance of this logger is inadequate for production usage.
 // It is merely here to showcase the callback usage.
 void myLogFunc (const void *user_data, uint32_t level, const char *message) {
+    // beware of potential close logger signal
+    if (!message) return;
     FILE *wh = ruFOpen((const char*) user_data, "a", NULL);
     if (wh) {
         fwrite(message, strlen(message), sizeof(char), wh);
